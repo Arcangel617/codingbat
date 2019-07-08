ce3522727e6bc42b564bc8b6ee5e66b02f749f5c
Arcangel Andres <artigue_arcangel@hotmail.com>
2019-07-08 01:41:21 -0300

    Add solved exercises

--- src/main/java/com/codingbat/String1.java
+++ src/main/java/com/codingbat/String1.java
@@ -18,4 +18,44 @@ public class String1 {
         return out.substring(0,2) + word + out.substring(2);
     }
 
+    public String extraEnd(String str) {
+        String lastTwoChars = str.substring(str.length()-2);
+        return lastTwoChars + lastTwoChars + lastTwoChars;
+    }
+
+    public String firstTwo(String str) {
+        return str.length() > 2 ? str.substring(0,2) : str;
+    }
+
+    public String firstHalf(String str) {
+        return str.substring(0, str.length() / 2);
+    }
+
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length()-1);
+    }
+
+    public String comboString(String a, String b) {
+        return a.length() > b.length() ? b + a + b : a + b + a;
+    }
+
+    public String nonStart(String a, String b) {
+        return a.substring(1) + b.substring(1);
+    }
+
+    public String left2(String str) {
+        return str.length() > 2 ? str.substring(2) + str.substring(0,2) : str;
+    }
+
+    public String right2(String str) {
+        return str.substring(str.length()-2) + str.substring(0, str.length()-2);
+    }
+
+    public String theEnd(String str, boolean front) {
+        return front ? str.substring(0,1) : str.substring(str.length()-1);
+    }
+
+    public String withouEnd2(String str) {
+        return str.length() > 2 ? str.substring(1, str.length()-1) : "";
+    }
 }
--- src/test/java/com/codingbat/String1Test.java
+++ src/test/java/com/codingbat/String1Test.java
@@ -41,4 +41,104 @@ public class String1Test {
         assertEquals(expected, result);
     }
 
+    @TestWith({
+            "Hello, lololo",
+            "ab   , ababab",
+            "Hi   , HiHiHi"
+    })
+    public void shouldReturnExtraEnd(String str, String expected) {
+        String result = string1.extraEnd(str);
+        assertEquals(expected, result);
+    }
+
+    @TestWith({
+            "Hello  , He",
+            "abcdefg, ab",
+            "ab     ,ab",
+            "a      , a"
+    })
+    public void shouldReturnFirstTwo(String str, String expected) {
+        String result = string1.firstTwo(str);
+        assertEquals(expected, result);
+    }
+
+    @TestWith({
+            "WooHoo    , Woo",
+            "HelloThere, Hello",
+            "abcdef    , abc"
+    })
+    public void shouldReturnFirstHalf(String str, String expected) {
+        String result = string1.firstHalf(str);
+        assertEquals(expected, result);
+    }
+
+    @TestWith({
+            "Hello , ell",
+            "java  , av",
+            "coding, odin"
+    })
+    public void shouldReturnWithoutEnd(String str, String expected) {
+        String result = string1.withoutEnd(str);
+        assertEquals(expected, result);
+    }
+
+    @TestWith({
+            "Hello, hi   , hiHellohi",
+            "hi   , Hello, hiHellohi",
+            "aaa  , b    , baaab"
+    })
+    public void shouldReturnComboString(String a, String b, String expected) {
+        String result = string1.comboString(a, b);
+        assertEquals(expected, result);
+    }
+
+    @TestWith({
+            "Hello, There, ellohere",
+            "java , code , avaode",
+            "shotl, java , hotlava"
+    })
+    public void shouldReturnNonStart(String a, String b, String expected) {
+        String result = string1.nonStart(a, b);
+        assertEquals(expected, result);
+    }
+
+    @TestWith({
+            "Hello, lloHe",
+            "java , vaja",
+            "Hi   , Hi"
+    })
+    public void shouldReturnLeft2(String str, String expected) {
+        String result = string1.left2(str);
+        assertEquals(expected, result);
+    }
+
+    @TestWith({
+            "Hello, loHel",
+            "java , vaja",
+            "Hi   , Hi"
+    })
+    public void shouldRight2(String str, String expected) {
+        String result = string1.right2(str);
+        assertEquals(expected, result);
+    }
+
+    @TestWith({
+            "Hello, true , H",
+            "Hello, false, o",
+            "ohn  , true , o"
+    })
+    public void shouldReturnTheEnd(String str, boolean front, String expected) {
+        String result = string1.theEnd(str, front);
+        assertEquals(expected, result);
+    }
+
+    @TestWith({
+            "Hello, ell",
+            "abc  , b",
+            "ab   , "
+    })
+    public void shouldReturnTheEnd2(String str, String expected) {
+        String result = string1.withouEnd2(str);
+        assertEquals(expected, result);
+    }
 }
